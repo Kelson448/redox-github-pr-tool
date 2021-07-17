537de102e8d1ed815922cdaa42122277e8fce356
Kelson448 <kelson448@gmail.com>
2021-07-17 17:52:40 -0600

    Futher setup of graphql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function helloWorld(){
-	return 'Hello World!'
+import { GraphQLClient, gql } from 'graphql-request'
+import { GITHUB_TOKEN } from './token'
+
+const url = 'https://api.github.com/graphql'
+const client = new GraphQLClient(url, {headers:{
+	authorization: `Bearer ${GITHUB_TOKEN}`
+}})
+
+// fetch PRs
+export async function fetchPRs(){
+	const query = gql``
+
+	return await client.request(query)
 }
+// type created for returned value
+
+// store PRs
