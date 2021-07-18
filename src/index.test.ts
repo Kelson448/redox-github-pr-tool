7db506c943ce5b1e508c41d2628bee8ab841e8d0
Kelson448 <kelson448@gmail.com>
2021-07-17 19:29:28 -0600

    Pulling in PRs from github

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,8 @@
-import { fetchPRs } from ".";
-
+import { getPullRequests } from ".";
+jest.setTimeout(20000)
 describe('index', ()=>{
-	it('fetchPRs runs', ()=>{
-		fetchPRs()
+	it('fetchPRs runs', async ()=>{
+		const results = await getPullRequests()
+		console.log(results)
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,61 @@
-import { GraphQLClient, gql } from 'graphql-request'
-import { GITHUB_TOKEN } from './token'
+import { GraphQLClient, gql } from "graphql-request";
+import { GITHUB_TOKEN } from "./token";
 
-const url = 'https://api.github.com/graphql'
-const client = new GraphQLClient(url, {headers:{
-	authorization: `Bearer ${GITHUB_TOKEN}`
-}})
+const url = "https://api.github.com/graphql";
+const client = new GraphQLClient(url, {
+	headers: {
+		authorization: `Bearer ${GITHUB_TOKEN}`,
+	},
+});
+
+interface PullRequest {
+	author: {
+		login: string;
+	};
+	publishedAt: string;
+	state: string;
+	title: string;
+}
 
 // fetch PRs
-export async function fetchPRs(){
-	const query = gql``
+export async function getPullRequests() {
+	const query = gql`
+		query getPullRequests($cursor: String) {
+			repository(name: "ramda", owner: "ramda") {
+				pullRequests(first: 100, after: $cursor) {
+					nodes {
+						author {
+							login
+						}
+						publishedAt
+						state
+						title
+					}
+					pageInfo {
+						endCursor
+						hasNextPage
+					}
+					totalCount
+				}
+			}
+		}
+	`;
 
-	return await client.request(query)
+	// const totalPrs = 1644
+	let pullRequests: PullRequest[] = [];
+	let cursor: string | undefined = undefined;
+	let hasNextPage = true;
+	let totalCount: number = 0;
+
+	while (hasNextPage) {
+		const results: any = await client.request(query, { cursor: cursor });
+		hasNextPage = results.repository.pullRequests.pageInfo.hasNextPage;
+		cursor = results.repository.pullRequests.pageInfo.endCursor;
+		totalCount = results.repository.pullRequests.totalCount;
+		pullRequests = pullRequests.concat(results.repository.pullRequests.nodes);
+	}
+	console.log(pullRequests.length);
+	return pullRequests;
 }
 // type created for returned value
 
